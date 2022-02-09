0f03ab84968c5e4a32b4a7cd5fc4695671009c01
masappu <njzzmmi6tn@icloud.com>
2022-02-10 03:23:18 +0900

    プロジェクトの作成

---
+++ GitHubRequestAPIAPP/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GitHubRequestAPIAPP
+//
+//  Created by 山口誠士 on 2022/02/09.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
