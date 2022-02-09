d19b0c09b5db94d32e1eaafb80cc4c76bff76618
masappu <njzzmmi6tn@icloud.com>
2022-02-10 03:42:33 +0900

    ViewController/CustomCellのファイルを作成

---
+++ GitHubRequestAPIAPP/CustomCell/RepositoryCell.swift
@@ -0,0 +1,23 @@
+//
+//  RepositoryCell.swift
+//  GitHubRequestAPIAPP
+//
+//  Created by 山口誠士 on 2022/02/10.
+//
+
+import UIKit
+
+class RepositoryCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
---
+++ GitHubRequestAPIAPP/ViewController/RepositoryListsViewController.swift
@@ -0,0 +1,29 @@
+//
+//  RepositoryListsViewController.swift
+//  GitHubRequestAPIAPP
+//
+//  Created by 山口誠士 on 2022/02/10.
+//
+
+import UIKit
+
+class RepositoryListsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
