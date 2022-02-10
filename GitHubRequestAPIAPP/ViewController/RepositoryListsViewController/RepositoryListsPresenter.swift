4bbf751ae1c9c0e8988e3e2c679c0cc366c45d34
masappu <njzzmmi6tn@icloud.com>
2022-02-11 07:09:05 +0900

    RepositoryListsViewControllerの実装 RepositoryListsPresenterの実装 GitHubAPIModelの実装

--- GitHubRequestAPIAPP/ViewController/RepositoryListsViewController/RepositoryListsPresenter.swift
+++ GitHubRequestAPIAPP/ViewController/RepositoryListsViewController/RepositoryListsPresenter.swift
@@ -6,3 +6,91 @@
 //
 
 import Foundation
+
+protocol RepositoryListsPresenterInput{
+    
+    //画面表示用のデータ(リポジトリ検索結果)を共有
+    var repositorise:[Repository] {get}
+    
+    //viewDidLoadのタイミングを通知する
+    func loadView(SearchText:String)
+    
+    //pushLinkCopyButtonのタップを通知する
+    func pushLinkCopyButton(indexPath:IndexPath)
+    
+}
+
+protocol RepositoryListsPresenterOutput{
+    
+    //tableViewの設定を指示する
+    func setTableViewInfo()
+    
+    //activityIndicatorViewの設定を指示する
+    func setActivityIndicatorViewInfo()
+    
+    //activityIndicatorViewのstartAnimating()を指示する
+    func activityIndicatorViewStartAnimating()
+    
+    //activityIndicatorViewのstopAnimating()を指示する
+    func activityIndicatorViewStopAnimating()
+    
+    //Erroralert表示・全画面への遷移を指示する
+    func showErrorAndTransitionToToBackView(title:String, message:String)
+    
+    //クリップボードへのコピーが完了alertの表示を指示
+    func showAlertOfCompletedTextCopy(title:String)
+    
+    //tableViewのreloadを指示する。
+    func reloadData()
+    
+    //UIPasteboardの起動を指示する
+    func putTextToPasteboard(text:String)
+}
+
+
+
+final class RepositoryListsPresenter:RepositoryListsPresenterInput{
+    private var view:RepositoryListsPresenterOutput
+    var repositorise: [Repository] = []
+    
+    init(view:RepositoryListsPresenterOutput){
+        self.view = view
+    }
+    
+    func loadView(SearchText text: String) {
+        self.view.setTableViewInfo()
+        self.view.setActivityIndicatorViewInfo()
+        
+        self.view.activityIndicatorViewStartAnimating()
+        GitHubAPIMode.fetchRepositoryData(paramerter: text) { result in
+            switch result {
+            case .success(let repositories):
+                self.repositorise = repositories
+                DispatchQueue.main.async {
+                    self.view.activityIndicatorViewStopAnimating()
+                    self.view.reloadData()
+                }
+            case .failure(let error):
+                DispatchQueue.main.async {
+                    self.view.activityIndicatorViewStopAnimating()
+                    switch error {
+                    case .illgalWord:
+                        self.view.showErrorAndTransitionToToBackView(title: "不正な文字列です。", message: "検索ワードを確認してください。")
+                    case .netWorkError:
+                        self.view.showErrorAndTransitionToToBackView(title: "ネットワークに接続できません。", message: "ネットワーク接続を確認してください。")
+                    case .parseError:
+                        self.view.showErrorAndTransitionToToBackView(title: "予期せぬエラーが発生しました。", message: "")
+                    case .noData:
+                        self.view.showErrorAndTransitionToToBackView(title: "検索結果がありません。", message: "検索ワードを確認してください。")
+                    }
+                }
+            }
+        }
+    }
+    
+    func pushLinkCopyButton(indexPath: IndexPath) {
+        let text = self.repositorise[indexPath.row].html_url
+        self.view.putTextToPasteboard(text: text)
+        self.view.showAlertOfCompletedTextCopy(title: "クリップボードにコピーしました。")
+    }
+}
--- GitHubRequestAPIAPP/ViewController/RepositoryListsViewController/RepositoryListsViewController.swift
+++ GitHubRequestAPIAPP/ViewController/RepositoryListsViewController/RepositoryListsViewController.swift
@@ -9,45 +9,94 @@ import UIKit
 
 class RepositoryListsViewController: UIViewController {
     
-    @IBOutlet private weak var tableView: UITableView!{
-        didSet{
-            tableView.delegate = self
-            tableView.dataSource = self
-        }
+    @IBOutlet private weak var tableView: UITableView!
+    
+    var searchText:String!
+    private var presenter:RepositoryListsPresenterInput?
+    private var activityIndicatorView = UIActivityIndicatorView()
+    
+    func inject(presenter:RepositoryListsPresenterInput){
+        self.presenter = presenter
     }
     
-    var searchText:String?
-    
-    
-
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        let presenter = RepositoryListsPresenter(view: self)
+        inject(presenter: presenter)
+        self.presenter?.loadView(SearchText: self.searchText)
+    }
+    
+    @objc func pushLinkCopyButton(_ sender:UIButton){
+        let cell = sender.superview?.superview?.superview as! UITableViewCell
+        let indexPath = self.tableView.indexPath(for: cell)
+        self.presenter?.pushLinkCopyButton(indexPath: indexPath!)
+    }
+    
+}
 
-        // Do any additional setup after loading the view.
+extension RepositoryListsViewController:RepositoryListsPresenterOutput{
+    func setTableViewInfo() {
+        self.tableView.delegate = self
+        self.tableView.dataSource = self
+        self.tableView.allowsSelection = false
+        
         tableView.register(UINib(nibName: "RepositoryCell", bundle: nil), forCellReuseIdentifier: "repositoryCell")
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func setActivityIndicatorViewInfo() {
+        activityIndicatorView.center = view.center
+        activityIndicatorView.style = .medium
+        activityIndicatorView.color = .lightGray
+        activityIndicatorView.hidesWhenStopped = true
+        
+        view.addSubview(activityIndicatorView)
     }
-    */
-
+    
+    func activityIndicatorViewStartAnimating() {
+        self.activityIndicatorView.startAnimating()
+    }
+    
+    func activityIndicatorViewStopAnimating() {
+        self.activityIndicatorView.stopAnimating()
+    }
+    
+    func showErrorAndTransitionToToBackView(title: String, message: String) {
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        let ok = UIAlertAction(title: "OK", style: .default) { _ in
+            self.navigationController?.popViewController(animated: true)
+        }
+        alert.addAction(ok)
+        self.present(alert, animated: true, completion: nil)
+    }
+    
+    func showAlertOfCompletedTextCopy(title: String) {
+        let alert = UIAlertController(title: title, message: "", preferredStyle: .alert)
+        let ok = UIAlertAction(title: "OK", style: .default, handler: nil)
+        alert.addAction(ok)
+        self.present(alert, animated: true, completion: nil)
+    }
+    
+    func reloadData() {
+        self.tableView.reloadData()
+    }
+    
+    func putTextToPasteboard(text: String) {
+        UIPasteboard.general.string = text
+    }
 }
 
 extension RepositoryListsViewController:UITableViewDelegate,UITableViewDataSource{
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 3
+        return self.presenter?.repositorise.count ?? 0
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "repositoryCell") as! RepositoryCell
+        let data = self.presenter?.repositorise[indexPath.row]
+        cell.configure(repository: data!)
+        cell.copyUrlButton.addTarget(self, action: #selector(pushLinkCopyButton(_:)), for: .touchUpInside)
         return cell
     }
 }
